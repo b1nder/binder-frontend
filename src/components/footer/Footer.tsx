0737e3280e28be2edd657125a596a7cb259833c6
@u_limit_ <yurim@ajou.ac.kr>
2024-10-06 02:17:12 +0900

    style: 위치 지정

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,14 +5,33 @@ import PersonIcon from './svg/person.svg';
 
 import styled from '@emotion/styled';
 
+const FooterContainer = styled.div`
+  display: flex;
+  background-color: #f7fafc;
+  //width: 100%;
+`;
+
+const FooterContent = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  gap: 4rem;
+  width: 390px;
+  //@media (max-width: 600px) {
+  //  width: 90%; /* 휴대폰 크기일 때 width를 90%로 설정 */
+  //  gap: 2rem; /* 간격도 조정 가능 */
+  //}
+`;
+
 const FooterIconContainer = styled.footer`
   display: grid;
   flex-direction: column;
+  flex: 1;
   justify-items: center;
   cursor: pointer;
   color: #bdbdbd;
   gap: 8px;
-
+  margin: 12px;
   &:hover {
     color: #81c784;
     &:hover svg {
@@ -21,28 +40,27 @@ const FooterIconContainer = styled.footer`
   }
 `;
 
-const FooterContainer = styled.div`
-  display: flex;
-  gap: 4rem;
-  justify-content: center;
-  background-color: #f7fafc;
+const textBox = styled.div`
+  margin: 0;
 `;
 
 function Footer() {
   return (
     <FooterContainer>
-      <FooterIconContainer>
-        <BinderIcon />
-        <p>바인더</p>
-      </FooterIconContainer>
-      <FooterIconContainer>
-        <PlusIcon />
-        <p>바인더 추가</p>
-      </FooterIconContainer>
-      <FooterIconContainer>
-        <PersonIcon />
-        <p>마이페이지</p>
-      </FooterIconContainer>
+      <FooterContent>
+        <FooterIconContainer>
+          <BinderIcon />
+          <p>바인더</p>
+        </FooterIconContainer>
+        <FooterIconContainer>
+          <PlusIcon />
+          <p>바인더 추가</p>
+        </FooterIconContainer>
+        <FooterIconContainer>
+          <PersonIcon />
+          <p>마이페이지</p>
+        </FooterIconContainer>
+      </FooterContent>
     </FooterContainer>
   );
 }
