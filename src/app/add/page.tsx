269cabe952491047b4c5e6c3995c3dd3c387a4fd
@u_limit_ <yurim@ajou.ac.kr>
2024-10-09 03:19:53 +0900

    feat: 바인더 추가 페이지 개발

---
+++ src/app/add/page.tsx
@@ -0,0 +1,88 @@
+'use client';
+import styled from '@emotion/styled';
+import ToggleComponent from '@/components/Toggle';
+import Button from '@/components/Button';
+export default function add() {
+  const Title = styled.span`
+    font-weight: bold;
+    font-size: 18px;
+  `;
+  const clickHandler = () => {
+    console.log('clicked');
+  };
+  const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-width: 390px;
+    align-items: center;
+  `;
+  const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  `;
+  const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    gap: 16px;
+  `;
+  const Input = styled.input`
+    background-color: #e8edf2;
+    border: 0;
+    width: 358px;
+    height: 56px;
+    border-radius: 12px;
+    padding: 16px;
+    &:focus {
+      outline: none;
+    }
+    ::placeholder {
+      color: #4f7396;
+    }
+  `;
+  const Select = styled.select`
+    padding: 16px;
+    border-radius: 12px;
+    background-color: #e8edf2;
+    border: 0;
+    color: #4f7396;
+    height: 56px;
+    &:focus {
+      outline: none;
+    }
+  `;
+  return (
+    <PageContainer>
+      <Title style={{ padding: '16px' }}>바인더 추가</Title>
+      <ToggleComponent />
+      <Content className={'content'}>
+        <InputContainer>
+          <Title>제목</Title>
+          <Input type={'text'} placeholder={'일정 이름'} />
+        </InputContainer>
+        <InputContainer>
+          <Title>기간</Title>
+          <p>시작 일자</p>
+          <p>종료 일자</p>
+        </InputContainer>
+        <InputContainer>
+          <Title>타입</Title>
+          <Select id={'type'}>
+            <option value={'WE'}>WE</option>
+            <option value={'ME'}>ME</option>
+          </Select>
+        </InputContainer>
+        <InputContainer>
+          <Title>문화생활</Title>
+          <p>카테고리</p>
+          <p>티켓 첨부하기</p>
+          <p>사진 첨부하기</p>
+          <p>장소 등록</p>
+        </InputContainer>
+      </Content>
+
+      <Button content="추가" onClickHandler={clickHandler} width={'358px'} />
+    </PageContainer>
+  );
+}
