4ebb4b6242131ae4459876f5a69c407e5385df7a
@u_limit_ <yurim@ajou.ac.kr>
2024-10-09 03:20:38 +0900

    feat: button width props 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 type Props = {
   content: string;
   onClickHandler: (e: React.MouseEvent) => void;
+  width?: string;
 };
 
-const ButtonComponent = styled.button`
+const ButtonComponent = styled.button<{ width?: string }>`
   background-color: #81c784;
   font-size: 16px;
   color: white;
@@ -13,14 +14,24 @@ const ButtonComponent = styled.button`
   border: 0;
   white-space: nowrap;
   padding: 16px 36px;
+  width: ${(props) => props.width || 'auto'};
   &:hover {
     cursor: pointer;
   }
 `;
-export default function Button({ content, onClickHandler }: Props) {
+export default function Button({ content, onClickHandler, width }: Props) {
   return (
-    <div>
-      <ButtonComponent onClick={onClickHandler}>{content}</ButtonComponent>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <ButtonComponent onClick={onClickHandler} width={width}>
+        {content}
+      </ButtonComponent>
     </div>
   );
 }
